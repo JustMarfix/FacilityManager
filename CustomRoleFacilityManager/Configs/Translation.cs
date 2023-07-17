42996ecaea22bcb9ad95a7b2a15fc383f4030f9b
soufi <47915535+soufied@users.noreply.github.com>
2023-07-17 22:51:26 +0300

    Add files via upload

--- CustomRoleFacilityManager/Configs/Translation.cs
+++ CustomRoleFacilityManager/Configs/Translation.cs
@@ -7,6 +7,7 @@ namespace CustomRoleFacilityManager.Configs
     {
         public string CustomName { get; set; } = "Facility Manager";
         public string CustomInfo { get; set; } = "Facility Manager";
+        public string Spawn { get; set; } = "You have spawned as a <color=red>Facility Manager</color>";
         public string Description { get; set; } = "You are <color=red>Facility Manager</color>, check console for more info (~)";
         public string ConsoleMsg { get; set; } = "<size=60%>You are <color=red>Facility Manager</color></size>";
     }
